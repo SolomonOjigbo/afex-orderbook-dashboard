2f09f07a2150cc769be7a372f34de807ba274c2f
SolomonOjigbo <solomonojigbo@gmail.com>
2023-01-31 11:58:09 +0100

    Designed the Orderbook side navigation menu

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,8 +1,44 @@
 import React from "react";
-import styles from "./Sidemenu.module.scss";
+import styles from "./SideMenu.module.scss";
+import { BiTrendingUp } from "react-icons/bi";
+import { FiBook, FiSearch } from "react-icons/fi";
+import { RxCounterClockwiseClock } from "react-icons/rx";
+import { AiOutlineEye } from "react-icons/ai";
+import { MdOutlineCheckCircleOutline, MdOutlineCancel } from "react-icons/md";
 
 const SideMenu = () => {
-	return <div>Sidemenu</div>;
+	return (
+		<div className={styles.container}>
+			<div className={styles.search}>
+				<FiSearch className={styles.icon} color="grey" />
+				<input type="text" placeholder="Search" />
+			</div>
+			<div className={styles.menu_item}>
+				<BiTrendingUp size="18px" />
+				<span>Product View</span>
+			</div>
+			<div className={styles.menu_item}>
+				<FiBook size="18px" />
+				<span style={{ color: "red" }}>Order Book</span>
+			</div>
+			<div className={styles.menu_item}>
+				<RxCounterClockwiseClock size="18px" />
+				<span>Price History</span>
+			</div>
+			<div className={styles.menu_item}>
+				<AiOutlineEye size="18px" />
+				<span>Open Orders</span>
+			</div>
+			<div className={styles.menu_item}>
+				<MdOutlineCheckCircleOutline size="18px" />
+				<span>Closed Trades</span>
+			</div>
+			<div className={styles.menu_item}>
+				<MdOutlineCancel size="18px" />
+				<span>Cancelled Trades</span>
+			</div>
+		</div>
+	);
 };
 
 export default SideMenu;
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,38 @@
 import React from "react";
 import styles from "./Sidebar.module.scss";
+import { BsColumnsGap, BsGraphUp } from "react-icons/bs";
+import { FiBriefcase, FiUsers, FiSettings } from "react-icons/fi";
+import { GrDocument } from "react-icons/gr";
 
 const Sidebar = () => {
-	return <div>Sidebar</div>;
+	return (
+		<div className={styles.container}>
+			<div className={styles.menu_item}>
+				<BsColumnsGap size="25px" />
+				<span>Overview</span>
+			</div>
+			<div className={styles.menu_item}>
+				<BsGraphUp size="25px" color="red" />
+				<span>Market</span>
+			</div>
+			<div className={styles.menu_item}>
+				<FiBriefcase size="25px" />
+				<span>Portfolio</span>
+			</div>
+			<div className={styles.menu_item}>
+				<FiUsers size="25px" />
+				<span>Community</span>
+			</div>
+			<div className={styles.menu_item}>
+				<GrDocument size="25px" />
+				<span>Report</span>
+			</div>
+			<div className={styles.menu_item}>
+				<FiSettings size="25px" />
+				<span>Settings</span>
+			</div>
+		</div>
+	);
 };
 
 export default Sidebar;
