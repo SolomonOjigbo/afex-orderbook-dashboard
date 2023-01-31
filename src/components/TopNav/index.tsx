6b4701adf2f1ff9bcad197a5786c6eafbec1266d
SolomonOjigbo <solomonojigbo@gmail.com>
2023-01-31 13:28:25 +0100

    Designed the Orderbook Topmenu

--- src/components/TopNav/index.tsx
+++ src/components/TopNav/index.tsx
@@ -1,8 +1,42 @@
 import React from "react";
 import styles from "./TopNav.module.scss";
+import { Button } from "@mui/material";
 
 const TopNav = () => {
-	return <div>TopNav</div>;
+	return (
+		<div className={styles.container}>
+			<div className={styles.row}>
+				<p>Board</p>
+
+				<button
+					className={styles.button}
+					style={{ backgroundColor: "#e00404", color: "white" }}
+				>
+					X-Trade
+				</button>
+
+				<button className={styles.button}>OTC</button>
+				<button className={styles.button}>FI</button>
+				<button className={styles.button}>Derivatives</button>
+			</div>
+			<div className={styles.row}>
+				<p>Product</p>
+				<button
+					className={styles.button}
+					style={{ backgroundColor: "#e00404", color: "white" }}
+				>
+					ALL
+				</button>
+				<button className={styles.button}>SMAZ</button>
+				<button className={styles.button}>SBBS</button>
+				<button className={styles.button}>SPRL</button>
+				<button className={styles.button}>SSGN</button>
+				<button className={styles.button}>SBGM</button>
+				<button className={styles.button}>FETC</button>
+				<button className={styles.button}>SCOC</button>
+			</div>
+		</div>
+	);
 };
 
 export default TopNav;
